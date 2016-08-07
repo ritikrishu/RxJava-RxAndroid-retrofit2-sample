20c439bd402df7afe7b7aab01945beed02092d0d
ritikrishu <ritik.rishu@hotcocoasotware.com>
2016-08-07 23:13:06 +0530

    first time rx java is working atleast

--- app/src/main/java/com/ritikrishu/weatherapp/API/Service.java
+++ app/src/main/java/com/ritikrishu/weatherapp/API/Service.java
@@ -7,6 +7,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -14,21 +15,24 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class Service {
     public static API.GETDATA hitRetro(){
-//        OkHttpClient client = new OkHttpClient.Builder()
-//                .addInterceptor(new Interceptor() {
-//                    @Override
-//                    public Response intercept(Chain chain) throws IOException {
-//                        Request request = chain.request()
-//                                .newBuilder()
-//                                .build();
-//                        return chain.proceed(request);
-//                    }
-//                }).build();
 
-        return new Retrofit.Builder()
-                .baseUrl(API.BASE_URL)
-                .client(new OkHttpClient())
+        Retrofit.Builder builder = new Retrofit.Builder().addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(API.BASE_URL);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request()
+                                .newBuilder()
+                                .build();
+                        return chain.proceed(request);
+                    }
+                }).build();
+        builder.client(client);
+
+        return builder
                 .build()
                 .create(API.GETDATA.class);
     }
